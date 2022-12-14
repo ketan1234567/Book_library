1dfdb2fd0a8bc427fadef5921712cb583feee17d
ketandeshmukh <deshmukhketan647@gmail.com>
2022-12-14 16:51:14 +0530

    "This is start"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddbookComponent } from './addbook/addbook.component';
+import { HomeComponent } from './home/home.component';
+import { ManagebookComponent } from './managebook/managebook.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddbookComponent,
+    HomeComponent,
+    ManagebookComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/managebook/managebook.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-managebook',
+  templateUrl: './managebook.component.html',
+  styleUrls: ['./managebook.component.css']
+})
+export class ManagebookComponent {
+
+}
